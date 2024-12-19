1f8e0677c4e88b2cfa7f74a949dde932448c5487
lazodev <solustec.dev@gmail.com>
2024-12-19 02:39:34 -0300

    estudos em java

---
+++ TestesIniciais.java
@@ -0,0 +1,31 @@
+public class OlaMergulhador {
+    
+    public static void main (String[] args) {
+        System.out.println("olá, programador java!");
+
+        String nomeCompleto = "José Sebastião";
+        nomeCompleto = "João da Silva";
+
+        System.out.println("Nome: " + nomeCompleto);
+
+        // declarando variavel do tipo inteiro
+        int minhaIdade = 41;
+        int suaIdade = 25;
+        int totalIdades = minhaIdade + sua idade;
+        System.out.println(totalIdades);
+
+//      declarando variavel do tipo double de ate 64 bits
+        double peso = 84.9d;
+        System.out.println(peso);
+
+        //declarando variavel do tipo flutuante de ate 32bits
+        float taxa = 1_294.93f;
+
+        //
+        boolean compraAprovada = false;
+        System.out.println(compraAprovada);
+
+        boolean maiorDeIdade = minhaIdade > 18;
+        System.out.println(maiorDeIdade);
+    }
+}
